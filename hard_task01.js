fc1ab3f392a55355e40f0a6a12ba6ae8f540ac94
Aleksandr-Viktorovich <teanvkz@gmail.com>
2023-01-30 01:46:06 +0700

    hard

---
+++ hard_task01.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const allCashbox = [
+  [12, 4500],
+  [7, 3210],
+  [4, 650],
+  [3, 1250],
+  [9, 7830],
+  [1, 990],
+  [6, 13900],
+  [1, 370],
+];
+
+for (let i = 0; i < allCashbox.length; i++) {
+  console.log(allCashbox[i][1] / allCashbox[i][0])
+}
+
+
+
+
+
